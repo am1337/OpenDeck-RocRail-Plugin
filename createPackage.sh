a071a8e479acb99f0ba2656392a01a586967f7ab
Achim Marikar <info@marikar.com>
2026-06-22 20:13:22 +0200

    deleted temp files

--- createPackage.sh
+++ createPackage.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+
+cd com.marikar.rocrailcontrol.sdPlugin
+npm install
+cd -
+
 rm -f com.marikar.rocrailcontrol.streamDeckPlugin && zip -r com.marikar.rocrailcontrol.streamDeckPlugin com.marikar.rocrailcontrol.sdPlugin
+
